ff8fea38c589191501ce10fc669237e1d4487450
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-06 23:45:23 +0900

    ss

--- gugudan.js
+++ gugudan.js
@@ -6,3 +6,20 @@ for (let i = 2; i <= 9; i++) {
     console.log(i + " * " + j + " = " + i * j);
   }
 }
+
+
+//구구단 전체출력 이중포문
+
+for (int j = 1; j < 10; j++) {
+	
+	for (int i = 1; i < 10; i++) {
+		System.out.printf("%d * %d = %2d   ", i, j, i * j);
+	}
+	System.out.printf("%n");
+}
+
+
+let arr = ['a', 'b', 'c'];
+
+// arr = ['d', 'a', 'b', 'c']
+arr.unshift('d');
