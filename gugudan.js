3b31c1cc9647fe1ba64efc5ad5ba19d717aa69f1
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-07 23:52:31 +0900

    vvv

--- gugudan.js
+++ gugudan.js
@@ -16,3 +16,11 @@ var arr2 = ["a", "b", "c"];
 
 // arr = ['a', 'b', 'c', 'd']
 arr.push("d"); // 배열의 끝에 요소를 추가
+
+var arr3 = ["a", "b", "c"];
+
+// arr = ['a', 'b', 'd', 'c']
+arr.splice(2, 0, "d"); // index 2 ('c')의 위치에 요소를 추가
+
+// arr = ['a', 'b', 'd', 'c', 'e', 'f']
+arr.splice(4, 0, "e", "f"); // index 4의 위치에 2개의 요소를 추가
