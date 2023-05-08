d6a7b0e073fd606214856f2ce98c04d82b307752
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-08 23:30:02 +0900

    v

--- splice.js
+++ splice.js
@@ -1,10 +1,10 @@
 let arr3 = ["a", "b", "c"];
 
 // arr = ['a', 'b', 'd', 'c']
-arr.splice(2, 0, "d"); // index 2 ('c')의 위치에 요소를 추가
+arr3.splice(2, 0, "d"); // index 2 ('c')의 위치에 요소를 추가
 
 // arr = ['a', 'b', 'd', 'c', 'e', 'f']
-arr.splice(4, 0, "e", "f"); // index 4의 위치에 2개의 요소를 추가
+arr3.splice(4, 0, "e", "f"); // index 4의 위치에 2개의 요소를 추가
 
 //배열 요소를 삭제하는 방법
 
@@ -17,3 +17,13 @@ arr.pop(); // 배열의 마지막 요소를 제거
 let popped = arr.pop(); // 제거한 요소를 반환 받을 수 있음
 
 // popped = 'e'
+
+let arr4 = ["a", "b", "c", "e", "f"];
+
+// arr = ['b', 'c', 'e', 'f']
+arr4.shift(); // 배열의 첫번째 요소를 제거
+
+// arr = ['c', 'e', 'f']
+let shifted = arr4.shift(); // 제거한 요소를 반환 받을 수 있음
+
+// shifted = 'b'
