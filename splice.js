15cb22008b3404c06bce8d852048f198cde5e523
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-09 21:25:14 +0900

    first

--- splice.js
+++ splice.js
@@ -27,3 +27,8 @@ arr4.shift(); // 배열의 첫번째 요소를 제거
 let shifted = arr4.shift(); // 제거한 요소를 반환 받을 수 있음
 
 // shifted = 'b'
+
+let arr5 = ["a", "b", "c", "e", "f"];
+
+// arr = ['a', 'b', 'e', 'f']
+arr5.splice(2, 1); // index 2 부터 1개의 요소('c')를 제거
