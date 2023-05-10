0baccfb58e55d54c5e90c28e7c4c437de82c36c4
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-10 22:51:42 +0900

    vvvv

--- sle2.js
+++ sle2.js
@@ -2,3 +2,6 @@ let arr5 = ["a", "b", "c", "e", "f"];
 
 // arr = ['a', 'b', 'e', 'f']
 arr5.splice(2, 1); // index 2 부터 1개의 요소('c')를 제거
+
+// arr = ['a', 'f']
+arr5.splice(1, 2); // index 1 부터 2개의 요소('b', 'e')를 제거
