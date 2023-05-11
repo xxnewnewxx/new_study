4310edd4799fb13eab80e267ffa237879a27710b
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-11 22:20:21 +0900

    sle2 update2

--- sle2.js
+++ sle2.js
@@ -10,3 +10,7 @@ arr5.splice(1, 2); // index 1 부터 2개의 요소('b', 'e')를 제거
 removed = arr5.splice(1, 1); // 제거한 요소를 반환 받을 수 있음
 
 // removed = 'f'
+let arr6 = ["a", "b", "c", "e", "f"];
+
+// arr = ["a", undefined, "c", "e", "f"]
+delete arr6[1]; // delete로 배열을 삭제할 경우 요소는 그대로 존재하며 값만 삭제 됨
