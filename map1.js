c54fcb3a53dbb862993bed8d055be8559c98aeda
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-05-15 23:33:04 +0900

    map up2

--- map1.js
+++ map1.js
@@ -5,3 +5,16 @@ let modifiedArr = arr.map(function (element) {
 });
 
 console.log(modifiedArr); // [9, 12, 15, 18]
+
+let users = [
+  { firstName: "Susan", lastName: "Steward" },
+  { firstName: "Daniel", lastName: "Longbottom" },
+  { firstName: "Jacob", lastName: "Black" },
+];
+
+let userFullnames = users.map(function (element) {
+  return `${element.firstName} ${element.lastName}`;
+});
+
+console.log(userFullnames);
+// ["Susan Steward", "Daniel Longbottom", "Jacob Black"]
